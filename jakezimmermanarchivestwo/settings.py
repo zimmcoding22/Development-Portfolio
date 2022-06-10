69b2b0b2ed3b0522d9c8add736d2368fb90367a9
Jake Zimmerman <jakezimmerman@jakes-mbp.myfiosgateway.com>
2022-06-10 15:10:46 -0400

    test 2

--- jakezimmermanarchivestwo/settings.py
+++ jakezimmermanarchivestwo/settings.py
@@ -139,3 +139,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("test")
