e86686a08d61ac5eaa1179d508f5dfe52bb6cdce
Jake Zimmerman <jakezimmerman@jakes-mbp.myfiosgateway.com>
2022-06-10 15:59:12 -0400

    new stuff

--- jakezimmermanarchivestwo/settings.py
+++ jakezimmermanarchivestwo/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #own
+    'test_app_two',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'jakezimmermanarchivestwo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- test_app_two/views.py
+++ test_app_two/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+#import requests
 
 # Create your views here.
+
+
+def test_view(request, *args, **kwargs):
+	return render(request, "test.html")
