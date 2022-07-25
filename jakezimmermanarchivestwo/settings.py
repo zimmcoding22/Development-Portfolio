56bb35fd452380b3c2d9b5a01faa95d944f96dcf
Jake Zimmerman <jakezimmerman@jakes-mbp.myfiosgateway.com>
2022-07-25 19:11:37 -0400

    trying to work without debug

--- jakezimmermanarchivestwo/settings.py
+++ jakezimmermanarchivestwo/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-fbi&^#!e9yjfb!p@!grfv*1wm*casy7&no3a53*v0e66eo&ms9'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"
+#DEBUG = str(os.environ.get('DEBUG')) == "1"
+DEBUG = str(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ["jzarchivestwo.herokuapp.com"]
 
