893f09abafd7298713aa3a60e5fca2143eb2866f
Jake Zimmerman <jakezimmerman@jakes-mbp.myfiosgateway.com>
2022-07-26 11:53:00 -0400

    mailgun

--- jakezimmermanarchivestwo/settings.py
+++ jakezimmermanarchivestwo/settings.py
@@ -135,10 +135,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = "jakeziscoding@gmail.com"
-EMAIL_HOST_PASSWORD = "coding++2240"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = "jakeziscoding@gmail.com"
+# EMAIL_HOST_PASSWORD = "coding++2240"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
