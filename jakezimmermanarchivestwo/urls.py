041c6381f82799ff2010367819195a7e31b015cc
Jake Zimmerman <jakezimmerman@jakes-mbp.myfiosgateway.com>
2022-11-04 14:22:24 -0400

    fixed admin url

--- jakezimmermanarchivestwo/urls.py
+++ jakezimmermanarchivestwo/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.base_view, name='base'),
     path('cards/', views.cards_view),
     path('cards/api/', views.cards_api_view)
